a01d678dca77c2ce1ddfafbc4cef5997cf67347e
Elinor <oncelinor@gmail.com>
2022-06-18 16:24:37 +0200

    feat: Initial commit

---
+++ src/components/atoms/Test.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Test = (props) => {
+  return <div className="text-3xl font-bold underline">Test</div>;
+};
+
+Test.propTypes = {};
+
+export default Test;
